612057f8a0ab530678eec92c3cf783e9f7fdf33b
benz <benz.github@gmail.com>
2025-03-16 00:58:58 +0000

    v1.1 Fixing script logic

--- scrcpy.ps1
+++ scrcpy.ps1
@@ -3,21 +3,21 @@
 # to find it run `echo $PROFILE`
 function scrcpy {
     # Change this to your personal paths
-    $scrcpy_move = "./scrcpy-move.ahk"
-    $ahk_exe     = "C:/Program Files/AutoHotkey/v2/AutoHotkey.exe"
+    $scrcpy_move = "$PSScriptRoot/scrcpy-move.ahk"                  # path relative to this script
+    $ahk_exe     = "C:/Program Files/AutoHotkey/v2/AutoHotkey.exe"  # default installation path
 
-    # this is Optional for some default args
+    # This is optional for some default args
     if ($args[0] -eq "--") { $scrcpyArgs = $args }
     else { $scrcpyArgs = @("--window-borderless", "--turn-screen-off", "--stay-awake") + $args }
 
     # Start the AHK script in the background
     $scrcpy_move = Start-Process -FilePath "$ahk_exe" -ArgumentList "`"$scrcpy_move`"" -PassThru
 
-    # Start Scrcpy and wait until it exits
-    scrcpy.exe @scrcpyArgs
+    # Start scrcpy and wait until it exits
+    try { scrcpy.exe @scrcpyArgs }
 
-    # Kill the AHK script when Scrcpy exits
-    Stop-Process -Id $scrcpy_move.Id -ErrorAction SilentlyContinue
+    # Kill the AHK script when scrcpy exits
+    finally { Stop-Process -Id $scrcpy_move.Id -ErrorAction SilentlyContinue }
 }
 
-function:scrcpy @args
+scrcpy @args
